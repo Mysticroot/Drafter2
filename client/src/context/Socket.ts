8894cf9bb52f7abd996f4be79190170d67f5dde6
Aashu <Aashupatil218@gmail.com>
2026-03-09 19:57:17 +0530

    some images added

--- client/src/context/Socket.ts
+++ client/src/context/Socket.ts
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
-
-export const socket = io(import.meta.env.VITE_SERVER_URL, {
+export const socket = io("http://localhost:5000", {
   transports: ["websocket"],
   autoConnect: false,
 });
+//http://localhost:5000
+
+//import.meta.env.VITE_SERVER_URL
